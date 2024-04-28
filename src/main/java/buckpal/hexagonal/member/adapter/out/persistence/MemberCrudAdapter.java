632d7a4ac813a13ff2b8e24c321af5c1094b90cc
jay <rlaakswo0687@gmail.com>
2024-04-28 22:24:49 +0900

    transfer commit test commit

--- src/main/java/buckpal/hexagonal/member/adapter/out/persistence/MemberCrudAdapter.java
+++ src/main/java/buckpal/hexagonal/member/adapter/out/persistence/MemberCrudAdapter.java
@@ -44,7 +44,6 @@ public class MemberCrudAdapter implements MemberCrudPort{
         member.updateTransferPassword(transferPasswordUpdateRequest.getTransferPassword());
 
 
-
         return member;
     }
 }
